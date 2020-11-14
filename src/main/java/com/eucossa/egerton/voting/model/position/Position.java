2356276bec12479e46a3ef7cea3d559ce8ce3e48
odinga96 <divadagnido96@gmail.com>
2020-11-14 18:19:41 +0300

    Used to represent a position a candidate is vying for

---
+++ src/main/java/com/eucossa/egerton/voting/model/position/Position.java
@@ -0,0 +1,13 @@
+package com.eucossa.egerton.voting.model.position;
+
+import lombok.Data;
+
+/**
+ * @author Odinga David
+ * otienodavidodinga@gmail.com
+ */
+@Data
+public class Position {
+    private int id;
+    private String name;
+}
