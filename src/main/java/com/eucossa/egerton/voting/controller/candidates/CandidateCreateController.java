0b474fc5e74b9c409bf0fc00953f633d9c821a8a
odinga96 <divadagnido96@gmail.com>
2020-11-15 17:55:50 +0300

    Implemented method to create a candidate

---
+++ src/main/java/com/eucossa/egerton/voting/controller/candidates/CandidateCreateController.java
@@ -0,0 +1,39 @@
+package com.eucossa.egerton.voting.controller.candidates;
+
+import com.eucossa.egerton.voting.model.candidate.Candidate;
+import com.eucossa.egerton.voting.services.ServicesManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Odinga David
+ * otienodavidodinga@gmail.com
+ */
+
+
+@RestController
+@RequestMapping(path = "/api/v1/add")
+public class CandidateCreateController {
+
+    private ServicesManager servicesManager;
+
+    @Autowired
+    public void setServicesManager(ServicesManager servicesManager) {
+        this.servicesManager = servicesManager;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addCandidate(@RequestBody Candidate candidate) {
+        Candidate candidate1 = servicesManager
+                .getCandidateService()
+                .saveCandidate(candidate);
+
+        return new ResponseEntity<>(candidate1, HttpStatus.CREATED);
+
+    }
+}
