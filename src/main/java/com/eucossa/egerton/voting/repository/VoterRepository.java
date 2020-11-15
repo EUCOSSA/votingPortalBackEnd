92bfaae93f819912f84b2fea5d9313eda621e4b4
odinga96 <divadagnido96@gmail.com>
2020-11-15 21:53:07 +0300

    Changed method to fetch single Voter

--- src/main/java/com/eucossa/egerton/voting/repository/VoterRepository.java
+++ src/main/java/com/eucossa/egerton/voting/repository/VoterRepository.java
@@ -10,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface VoterRepository extends JpaRepository<Voter, String> {
 
     boolean existsByRegNo(String regNo);
+
+    Voter findByRegNo(String regNo);
 }
